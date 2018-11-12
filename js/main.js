6a8caa869fc8ba62e292cbda1242b6a525ba09af
Luís Ferreira <luisraferreira@gmail.com>
2018-11-12 21:15:51 +0000

    upd: add tasks

--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ const tasksDiv = document.querySelector('.tasks');
 
 btnAdd.addEventListener('click', addTask);
 
-function addTask() {
-    var task = "";
-    if (input.value != '') {
-
-        const markup = `<div class="task"> ${input.value} </div>`;
-        tasksDiv.innerHTML = markup;
-
+document.querySelector('.inputTask').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        addTask();
     }
+});
+
+
+function addTask() {
+    var li = document.createElement("li");
+    var inputValue = document.querySelector('.inputTask').value;
+    var text = document.createTextNode(inputValue);
+    li.appendChild(text);
+
+
+    if (inputValue === "") {
+        alert("Por favor preencha o campo");
+    } else {
+        document.querySelector('.ulTasks').appendChild(li);
+    }
+
+    document.querySelector('.inputTask').value = "";
+
+
+    var span = document.createElement('span');
+    var icon = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(icon);
+    li.appendChild(span);
+
 }
+
 
 function deleteTaks() {
 
