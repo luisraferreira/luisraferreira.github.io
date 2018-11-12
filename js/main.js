ce4d25a6c4393bad1edff8c9f3f01c6e7f115fe9
Luís Ferreira <luisraferreira@gmail.com>
2018-11-12 18:43:38 +0000

    todo list upd

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 var btnAdd = document.querySelector('.addTask');
 var input = document.querySelector('.inputTask');
-var tasks = [];
+const tasksDiv = document.querySelector('.tasks');
+//var tasks = [];
 
 btnAdd.addEventListener('click', addTask);
 
 function addTask() {
-    tasks.push(input.value);
-    console.log(tasks)
+    var task = "";
+    if (input.value != '') {
+
+        const markup = `<div class="task"> ${input.value} </div>`;
+        tasksDiv.innerHTML = markup;
+
+    }
+}
+
+function deleteTaks() {
+
 }
