14a0392f223ab29333882e1dbc9aa62768fe2745
Luís Ferreira <luisraferreira@gmail.com>
2018-11-13 12:34:11 +0000

    upd: delete task

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var btnAdd = document.querySelector('.addTask');
 var input = document.querySelector('.inputTask');
-const tasksDiv = document.querySelector('.tasks');
-//var tasks = [];
+//const tasksDiv = document.querySelector('.tasks');//var tasks = [];
 
 btnAdd.addEventListener('click', addTask);
 
@@ -17,8 +16,10 @@ function addTask() {
     var li = document.createElement("li");
     var inputValue = document.querySelector('.inputTask').value;
     var text = document.createTextNode(inputValue);
-    li.appendChild(text);
+    var span = document.createElement('span');
+    var icon = document.createTextNode("\u00D7");
 
+    li.appendChild(text);
 
     if (inputValue === "") {
         alert("Por favor preencha o campo");
@@ -28,11 +29,26 @@ function addTask() {
 
     document.querySelector('.inputTask').value = "";
 
-
-    var span = document.createElement('span');
-    var icon = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(icon);
     li.appendChild(span);
 
+    //delete task
+    var closeBtn = document.querySelectorAll('.close');
+    //    closeBtn[i].addEventListener('click', deleteTask);
+    var ul = document.querySelector('.ulTasks');
+    var len = ul.children.length;
+    for (var i = 0; i < len; i++) {
+        //        (function (index) {
+        //            closeBtn[i].onclick = function (el) {
+        //                deleteTask(el);
+        //            }
+        //        })(i);
+        closeBtn[i].addEventListener('click', deleteTask);
+    }
+}
+
+
+var deleteTask = function (el) {
+    el.target.parentNode.style.display = "none";
 }
